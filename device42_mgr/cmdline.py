ae977a033488e8210cfa9751710da7263b58102a
Saurabh Hirani <saurabh.hirani@gmail.com>
2016-01-28 20:43:08 +0530

    updating cmd callbacks

--- device42_mgr/cmdline.py
+++ device42_mgr/cmdline.py
@@ -7,23 +7,28 @@ import click
 @click.group()
 @click.option('--configfile', envvar='D42_CONFIGFILE',
               help='device42_mgr config file. Default: env var D42_CONFIGFILE')
-def cli(**kwargs):
+@click.pass_context
+def cli(ctx, **kwargs):
   """ Manage device42 bulk operations """
   pass
 
 @cli.command()
 @click.argument('datafile')
-def create(**kwargs):
+@click.pass_context
+def create(ctx, **kwargs):
   """ Create device42 objects """
+  print kwargs
   pass
 
 @cli.command()
 @click.argument('datafile')
-@click.option('--cached', default=True,
+@click.option('--cache/--no-cache', default=True,
+              envvar='D42_CACHE_READS',
               help='Use cached data. Default: True')
 @click.option('--maxrecords', default=-1,
               help='Max records to show. Default: -1 - show all')
-def read(**kwargs):
+@click.pass_context
+def read(ctx, **kwargs):
   """ Read device42 objects """
   pass
 
@@ -31,13 +36,15 @@ def read(**kwargs):
 @click.argument('datafile')
 @click.option('--cached', default=True,
               help='Use cached data. Default: True')
-def update(**kwargs):
+@click.pass_context
+def update(ctx, **kwargs):
   """ Update device42 objects """
   pass
 
 @cli.command()
 @click.argument('datafile')
-def delete(**kwargs):
+@click.pass_context
+def delete(ctx, **kwargs):
   """ Delete device42 objects """
   pass
 
